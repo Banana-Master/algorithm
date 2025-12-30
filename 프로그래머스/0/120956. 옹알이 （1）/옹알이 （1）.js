691c442c3b3fba84073ddc99674825cfe711c6ee
김성호 <tjdgh0313@gmail.com>
2025-12-30 11:55:00 +0900

    [level 0] Title: 옹알이 (1), Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -0,0 +1,22 @@
+function solution(babbling) {
+    const babb = ["aya", "ye", "woo", "ma"];
+    let result = 0;
+    
+    babbling.forEach((word) => {
+    let x = word;
+
+    for (let y = 0; y < babb.length; y++) {
+        if (x.startsWith(babb[y])) {
+            x = x.slice(babb[y].length);
+            y = -1;
+            continue;
+        }
+    }
+
+        if (x.length === 0) {
+            result++;
+        }
+    });
+
+    return result;
+}
