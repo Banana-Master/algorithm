b315334bbbd09ddcb3786531d6aaf5e6f14538b6
김성호 <tjdgh0313@gmail.com>
2026-02-11 13:25:14 +0900

    [level 0] Title: 약수 구하기, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+import math
+
+def solution(n):
+    divisors = set()
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            divisors.add(i)
+            divisors.add(n // i)
+    return sorted(list(divisors))
