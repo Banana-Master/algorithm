ac16014316348d0c4920364fc5cc3fbf78f7a15c
김성호 <tjdgh0313@gmail.com>
2026-07-08 17:02:07 +0900

    [level 0] Title: 배열 만들기 5, Time: 0.68 ms, Memory: 47.8 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -2,7 +2,7 @@ function solution(intStrs, k, s, l) {
     const result = [];
     
     intStrs.forEach((n) => {
-        const cn = Number(n.split('').slice(s, s + l).join(''));
+        const cn = Number(n.slice(s, s + l));
         if (k < cn) {
             result.push(cn);
         }
